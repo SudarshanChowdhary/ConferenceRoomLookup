be8ad2efb58e05decff674b25de9ef939aa7aa18
Sudarshan <sudarshan_koyalkar@apple.com>
2016-07-15 18:01:51 +0530

    created the conferenceroomlookup and json data

--- js/controllers/conference-room-lookup-ctrl.js
+++ js/controllers/conference-room-lookup-ctrl.js
@@ -1,5 +1,10 @@
 'use strict'
-ConferenceRoomLookup.controller("ConferenceRoom", function($scope){
+ConferenceRoomLookup.controller("ConferenceRoom", function($scope, $http, services){
 	//alert("controller")
+    $http.get('package.json').success(function (data)
+    {
+        $scope.testAccounts = data;
+        $scope.selectedTestAccount = $scope.testAccounts[0];
+    });
 
 })
