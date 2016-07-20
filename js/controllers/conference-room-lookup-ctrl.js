04802b57a8a40685a13999fecf2ff629c8996496
SudarshanChowdhary <sudarshan@gmail.com>
2016-07-21 01:17:31 +0530

    word

--- js/controllers/conference-room-lookup-ctrl.js
+++ js/controllers/conference-room-lookup-ctrl.js
@@ -1,6 +1,6 @@
 'use strict'
 
-ConferenceRoomLookup.controller("ConferenceRoom", function($scope, siteService, buildingService, floorService, roomService){
+ConferenceRoomLookup.controller("ConferenceRoom", function($scope, siteService, buildingService, floorService, roomService, timeRangeService){
 	$scope.enableFloor= false;
 
 	$scope.siteOptions= siteService.get();
@@ -17,55 +17,12 @@ ConferenceRoomLookup.controller("ConferenceRoom", function($scope, siteService, 
 			{name: "6.15 minutes", value: "6.25"},{name: "6.30 minutes", value: "6.5"},	{name: "6.45 minutes", value: "6.75"},{name: "7 hour", value: "7"},{name: "7.15 minutes", value: "7.25"},{name: "7.30 minutes", value: "7.5"},
 			{name: "7.45 minutes", value: "7.75"},{name: "8 hour", value: "8"},	{name: "8.15 minutes", value: "8.25"},{name: "8.30 minutes", value: "8.5"},	{name: "8.45 minutes", value: "8.75"}
 
-
-
 		]};
 
-	$scope.timeFrom = {
-		model: null,
-		fromOptions:[
-			{name: "12:00 AM", value: "12:00 AM"},{name: "12:15 AM", value: "12:15 AM"},{name: "12:30 AM", value: "12:30 AM"},{name: "12:45 AM", value: "12:45 AM"},{name: "1:00 AM", value: "1:00 AM"},
-			{name: "1:15 AM", value: "1:15 AM"},{name: "1:30 AM", value: "1:30 AM"},{name: "1:45 AM", value: "1:45 AM"},{name: "2:00 AM", value: "2:00 AM"},{name: "2:15 AM", value: "2:15 AM"},{name: "2:30 AM", value: "2:30 AM"},{name: "2:45 AM", value: "2:45 AM"},{name: "3:00 AM", value: "3:00 AM"},
-			{name: "3:15 AM", value: "3:15 AM"},{name: "3:30 AM", value: "3:30 AM"},{name: "3.45 AM", value: "3.45 AM"},{name: "4:00 AM", value: "4:00 AM"},{name: "4:15 AM", value: "4:15 AM"},{name: "4:30 AM", value: "4:30 AM"},
-			{name: "4:45 AM", value: "4:45 AM"},{name: "5:00 AM", value: "5:00 AM"},{name: "5:15 AM", value: "5:15 AM"},{name: "5:30 AM", value: "5:30 AM"},{name: "5:45 AM", value: "5:45 AM"},{name: "6:00 AM", value: "6:00 AM"},
-			{name: "6:15 AM", value: "6:15 AM"},{name: "6:30 AM", value: "6:30 AM"},{name: "6:45 AM", value: "6:45 AM"},{name: "7:00 AM", value: "7:00 AM"},{name: "7:15 AM", value: "7:15 AM"},{name: "7:30 AM", value: "7:30 AM"},
-			{name: "7:45 AM", value: "7.45 AM"},{name: "8:00 AM", value: "8:00 AM"},{name: "8:15 AM", value: "8:15 AM"},{name: "8:30 AM", value: "8:30 AM"},{name: "8:45 AM", value: "8:45 AM"},
-			{name: "9:00 AM", value: "9:00 AM"},{name: "9:30 AM", value: "9:30 AM"},{name: "9:45 AM", value: "9:45 AM"},{name: "10:00 AM", value: "10:00 AM"},{name: "10:15 AM", value: "10:15 AM"},{name: "10:30 AM", value: "10:30 AM"},{name: "10:45 AM", value: "10:45 AM"},
-			{name: "11:00 AM", value: "11:00 AM"},{name: "11:15 AM", value: "11:15 AM"},{name: "11:30 AM", value: "11:30 AM"},{name: "11:45 AM", value: "11:45 AM"},
-			{name: "12:00 PM", value: "12:00 PM"},{name: "12:15 PM", value: "12:15 PM"},{name: "12:30 PM", value: "12:30 PM"},{name: "12:45 PM", value: "12:45 PM"},{name: "1:00 PM", value: "1:00 PM"},
-			{name: "1:15 PM", value: "1:15 PM"},{name: "1:30 PM", value: "1:30 PM"},{name: "1:45 PM", value: "1:45 PM"},{name: "2:00 PM", value: "2:00 PM"},{name: "2:15 PM", value: "2:15 PM"},{name: "2:30 PM", value: "2:30 PM"},{name: "2:45 PM", value: "2:45 PM"},{name: "3:00 PM", value: "3:00 PM"},
-			{name: "3:15 PM", value: "3:15 PM"},{name: "3:30 PM", value: "3:30 PM"},{name: "3.45 PM", value: "3.45 PM"},{name: "4:00 PM", value: "4:00 PM"},{name: "4:15 PM", value: "4:15 PM"},{name: "4:30 PM", value: "4:30 PM"},
-			{name: "4:45 PM", value: "4:45 PM"},{name: "5:00 PM", value: "5:00 PM"},{name: "5:15 PM", value: "5:15 PM"},{name: "5:30 PM", value: "5:30 PM"},{name: "5:45 PM", value: "5:45 PM"},{name: "6:00 PM", value: "6:00 PM"},
-			{name: "6:15 PM", value: "6:15 PM"},{name: "6:30 PM", value: "6:30 PM"},{name: "6:45 PM", value: "6:45 PM"},{name: "7:00 PM", value: "7:00 PM"},{name: "7:15 PM", value: "7:15 PM"},{name: "7:30 PM", value: "7:30 PM"},
-			{name: "7:45 PM", value: "7.45 PM"},{name: "8:00 PM", value: "8:00 PM"},{name: "8:15 PM", value: "8:15 PM"},{name: "8:30 PM", value: "8:30 PM"},{name: "8:45 PM", value: "8:45 PM"},
-			{name: "9:00 PM", value: "9:00 PM"},{name: "9:30 PM", value: "9:30 PM"},{name: "9:45 PM", value: "9:45 PM"},{name: "10:00 PM", value: "10:00 PM"},{name: "10:15 PM", value: "10:15 PM"},{name: "10:30 PM", value: "10:30 PM"},{name: "10:45 PM", value: "10:45 PM"},
-			{name: "11:00 PM", value: "11:00 PM"},{name: "11:15 PM", value: "11:15 PM"},{name: "11:30 PM", value: "11:30 PM"},{name: "11:45 PM", value: "11:45 PM"}
+	$scope.timeFrom = timeRangeService.getFromTimeOptions();
+	$scope.timeTo = timeRangeService.getToTimeOptions();
 
 
-		]};
-
-	$scope.timeTo = {
-		model: null,
-		toOptions:[
-			{name: "12:00 AM", value: "12:00 AM"},{name: "12:15 AM", value: "12:15 AM"},{name: "12:30 AM", value: "12:30 AM"},{name: "12:45 AM", value: "12:45 AM"},{name: "1:00 AM", value: "1:00 AM"},
-			{name: "1:15 AM", value: "1:15 AM"},{name: "1:30 AM", value: "1:30 AM"},{name: "1:45 AM", value: "1:45 AM"},{name: "2:00 AM", value: "2:00 AM"},{name: "2:15 AM", value: "2:15 AM"},{name: "2:30 AM", value: "2:30 AM"},{name: "2:45 AM", value: "2:45 AM"},{name: "3:00 AM", value: "3:00 AM"},
-			{name: "3:15 AM", value: "3:15 AM"},{name: "3:30 AM", value: "3:30 AM"},{name: "3.45 AM", value: "3.45 AM"},{name: "4:00 AM", value: "4:00 AM"},{name: "4:15 AM", value: "4:15 AM"},{name: "4:30 AM", value: "4:30 AM"},
-			{name: "4:45 AM", value: "4:45 AM"},{name: "5:00 AM", value: "5:00 AM"},{name: "5:15 AM", value: "5:15 AM"},{name: "5:30 AM", value: "5:30 AM"},{name: "5:45 AM", value: "5:45 AM"},{name: "6:00 AM", value: "6:00 AM"},
-			{name: "6:15 AM", value: "6:15 AM"},{name: "6:30 AM", value: "6:30 AM"},{name: "6:45 AM", value: "6:45 AM"},{name: "7:00 AM", value: "7:00 AM"},{name: "7:15 AM", value: "7:15 AM"},{name: "7:30 AM", value: "7:30 AM"},
-			{name: "7:45 AM", value: "7.45 AM"},{name: "8:00 AM", value: "8:00 AM"},{name: "8:15 AM", value: "8:15 AM"},{name: "8:30 AM", value: "8:30 AM"},{name: "8:45 AM", value: "8:45 AM"},
-			{name: "9:00 AM", value: "9:00 AM"},{name: "9:30 AM", value: "9:30 AM"},{name: "9:45 AM", value: "9:45 AM"},{name: "10:00 AM", value: "10:00 AM"},{name: "10:15 AM", value: "10:15 AM"},{name: "10:30 AM", value: "10:30 AM"},{name: "10:45 AM", value: "10:45 AM"},
-			{name: "11:00 AM", value: "11:00 AM"},{name: "11:15 AM", value: "11:15 AM"},{name: "11:30 AM", value: "11:30 AM"},{name: "11:45 AM", value: "11:45 AM"},
-			{name: "12:00 PM", value: "12:00 PM"},{name: "12:15 PM", value: "12:15 PM"},{name: "12:30 PM", value: "12:30 PM"},{name: "12:45 PM", value: "12:45 PM"},{name: "1:00 PM", value: "1:00 PM"},
-			{name: "1:15 PM", value: "1:15 PM"},{name: "1:30 PM", value: "1:30 PM"},{name: "1:45 PM", value: "1:45 PM"},{name: "2:00 PM", value: "2:00 PM"},{name: "2:15 PM", value: "2:15 PM"},{name: "2:30 PM", value: "2:30 PM"},{name: "2:45 PM", value: "2:45 PM"},{name: "3:00 PM", value: "3:00 PM"},
-			{name: "3:15 PM", value: "3:15 PM"},{name: "3:30 PM", value: "3:30 PM"},{name: "3.45 PM", value: "3.45 PM"},{name: "4:00 PM", value: "4:00 PM"},{name: "4:15 PM", value: "4:15 PM"},{name: "4:30 PM", value: "4:30 PM"},
-			{name: "4:45 PM", value: "4:45 PM"},{name: "5:00 PM", value: "5:00 PM"},{name: "5:15 PM", value: "5:15 PM"},{name: "5:30 PM", value: "5:30 PM"},{name: "5:45 PM", value: "5:45 PM"},{name: "6:00 PM", value: "6:00 PM"},
-			{name: "6:15 PM", value: "6:15 PM"},{name: "6:30 PM", value: "6:30 PM"},{name: "6:45 PM", value: "6:45 PM"},{name: "7:00 PM", value: "7:00 PM"},{name: "7:15 PM", value: "7:15 PM"},{name: "7:30 PM", value: "7:30 PM"},
-			{name: "7:45 PM", value: "7.45 PM"},{name: "8:00 PM", value: "8:00 PM"},{name: "8:15 PM", value: "8:15 PM"},{name: "8:30 PM", value: "8:30 PM"},{name: "8:45 PM", value: "8:45 PM"},
-			{name: "9:00 PM", value: "9:00 PM"},{name: "9:30 PM", value: "9:30 PM"},{name: "9:45 PM", value: "9:45 PM"},{name: "10:00 PM", value: "10:00 PM"},{name: "10:15 PM", value: "10:15 PM"},{name: "10:30 PM", value: "10:30 PM"},{name: "10:45 PM", value: "10:45 PM"},
-			{name: "11:00 PM", value: "11:00 PM"},{name: "11:15 PM", value: "11:15 PM"},{name: "11:30 PM", value: "11:30 PM"},{name: "11:45 PM", value: "11:45 PM"}
-
-
-		]};
 
 	$scope.loadBuilding= function(site){
 		if(!site){
@@ -93,6 +50,18 @@ ConferenceRoomLookup.controller("ConferenceRoom", function($scope, siteService, 
 			$scope.roomData = null;
 			$scope.floorData = null;
 		}else{
+
+			//ToDo: When only one service gives all the data
+			// angular.forEach($scope.buildingOptions.building, function(obj, index) {
+			// 	console.log(obj, index);
+
+					// angular.forEach(obj, function(obj, index) {
+						// 	console.log(obj, index);
+					// });
+
+			// });
+
+
 			angular.forEach($scope.buildingOptions.building, function(obj, value) {
 				if(value==building){
 					if(obj.floor)
---
+++ js/services/timeRange-service.js
@@ -0,0 +1,580 @@
+'use strict'
+ConferenceRoomLookup.factory("timeRangeService", function() {
+    return {
+        getFromTimeOptions: function() {
+            return [{
+                name: "12:00 AM",
+                value: "12:00 AM", 
+                id: 0
+            }, {
+                name: "12:15 AM",
+                value: "12:15 AM"
+            }, {
+                name: "12:30 AM",
+                value: "12:30 AM"
+            }, {
+                name: "12:45 AM",
+                value: "12:45 AM"
+            }, {
+                name: "1:00 AM",
+                value: "1:00 AM"
+            }, {
+                name: "1:15 AM",
+                value: "1:15 AM"
+            }, {
+                name: "1:30 AM",
+                value: "1:30 AM"
+            }, {
+                name: "1:45 AM",
+                value: "1:45 AM"
+            }, {
+                name: "2:00 AM",
+                value: "2:00 AM"
+            }, {
+                name: "2:15 AM",
+                value: "2:15 AM"
+            }, {
+                name: "2:30 AM",
+                value: "2:30 AM"
+            }, {
+                name: "2:45 AM",
+                value: "2:45 AM"
+            }, {
+                name: "3:00 AM",
+                value: "3:00 AM"
+            }, {
+                name: "3:15 AM",
+                value: "3:15 AM"
+            }, {
+                name: "3:30 AM",
+                value: "3:30 AM"
+            }, {
+                name: "3.45 AM",
+                value: "3.45 AM"
+            }, {
+                name: "4:00 AM",
+                value: "4:00 AM"
+            }, {
+                name: "4:15 AM",
+                value: "4:15 AM"
+            }, {
+                name: "4:30 AM",
+                value: "4:30 AM"
+            }, {
+                name: "4:45 AM",
+                value: "4:45 AM"
+            }, {
+                name: "5:00 AM",
+                value: "5:00 AM"
+            }, {
+                name: "5:15 AM",
+                value: "5:15 AM"
+            }, {
+                name: "5:30 AM",
+                value: "5:30 AM"
+            }, {
+                name: "5:45 AM",
+                value: "5:45 AM"
+            }, {
+                name: "6:00 AM",
+                value: "6:00 AM"
+            }, {
+                name: "6:15 AM",
+                value: "6:15 AM"
+            }, {
+                name: "6:30 AM",
+                value: "6:30 AM"
+            }, {
+                name: "6:45 AM",
+                value: "6:45 AM"
+            }, {
+                name: "7:00 AM",
+                value: "7:00 AM"
+            }, {
+                name: "7:15 AM",
+                value: "7:15 AM"
+            }, {
+                name: "7:30 AM",
+                value: "7:30 AM"
+            }, {
+                name: "7:45 AM",
+                value: "7.45 AM"
+            }, {
+                name: "8:00 AM",
+                value: "8:00 AM"
+            }, {
+                name: "8:15 AM",
+                value: "8:15 AM"
+            }, {
+                name: "8:30 AM",
+                value: "8:30 AM"
+            }, {
+                name: "8:45 AM",
+                value: "8:45 AM"
+            }, {
+                name: "9:00 AM",
+                value: "9:00 AM"
+            }, {
+                name: "9:30 AM",
+                value: "9:30 AM"
+            }, {
+                name: "9:45 AM",
+                value: "9:45 AM"
+            }, {
+                name: "10:00 AM",
+                value: "10:00 AM"
+            }, {
+                name: "10:15 AM",
+                value: "10:15 AM"
+            }, {
+                name: "10:30 AM",
+                value: "10:30 AM"
+            }, {
+                name: "10:45 AM",
+                value: "10:45 AM"
+            }, {
+                name: "11:00 AM",
+                value: "11:00 AM"
+            }, {
+                name: "11:15 AM",
+                value: "11:15 AM"
+            }, {
+                name: "11:30 AM",
+                value: "11:30 AM"
+            }, {
+                name: "11:45 AM",
+                value: "11:45 AM"
+            }, {
+                name: "12:00 PM",
+                value: "12:00 PM"
+            }, {
+                name: "12:15 PM",
+                value: "12:15 PM"
+            }, {
+                name: "12:30 PM",
+                value: "12:30 PM"
+            }, {
+                name: "12:45 PM",
+                value: "12:45 PM"
+            }, {
+                name: "1:00 PM",
+                value: "1:00 PM"
+            }, {
+                name: "1:15 PM",
+                value: "1:15 PM"
+            }, {
+                name: "1:30 PM",
+                value: "1:30 PM"
+            }, {
+                name: "1:45 PM",
+                value: "1:45 PM"
+            }, {
+                name: "2:00 PM",
+                value: "2:00 PM"
+            }, {
+                name: "2:15 PM",
+                value: "2:15 PM"
+            }, {
+                name: "2:30 PM",
+                value: "2:30 PM"
+            }, {
+                name: "2:45 PM",
+                value: "2:45 PM"
+            }, {
+                name: "3:00 PM",
+                value: "3:00 PM"
+            }, {
+                name: "3:15 PM",
+                value: "3:15 PM"
+            }, {
+                name: "3:30 PM",
+                value: "3:30 PM"
+            }, {
+                name: "3.45 PM",
+                value: "3.45 PM"
+            }, {
+                name: "4:00 PM",
+                value: "4:00 PM"
+            }, {
+                name: "4:15 PM",
+                value: "4:15 PM"
+            }, {
+                name: "4:30 PM",
+                value: "4:30 PM"
+            }, {
+                name: "4:45 PM",
+                value: "4:45 PM"
+            }, {
+                name: "5:00 PM",
+                value: "5:00 PM"
+            }, {
+                name: "5:15 PM",
+                value: "5:15 PM"
+            }, {
+                name: "5:30 PM",
+                value: "5:30 PM"
+            }, {
+                name: "5:45 PM",
+                value: "5:45 PM"
+            }, {
+                name: "6:00 PM",
+                value: "6:00 PM"
+            }, {
+                name: "6:15 PM",
+                value: "6:15 PM"
+            }, {
+                name: "6:30 PM",
+                value: "6:30 PM"
+            }, {
+                name: "6:45 PM",
+                value: "6:45 PM"
+            }, {
+                name: "7:00 PM",
+                value: "7:00 PM"
+            }, {
+                name: "7:15 PM",
+                value: "7:15 PM"
+            }, {
+                name: "7:30 PM",
+                value: "7:30 PM"
+            }, {
+                name: "7:45 PM",
+                value: "7.45 PM"
+            }, {
+                name: "8:00 PM",
+                value: "8:00 PM"
+            }, {
+                name: "8:15 PM",
+                value: "8:15 PM"
+            }, {
+                name: "8:30 PM",
+                value: "8:30 PM"
+            }, {
+                name: "8:45 PM",
+                value: "8:45 PM"
+            }, {
+                name: "9:00 PM",
+                value: "9:00 PM"
+            }, {
+                name: "9:30 PM",
+                value: "9:30 PM"
+            }, {
+                name: "9:45 PM",
+                value: "9:45 PM"
+            }, {
+                name: "10:00 PM",
+                value: "10:00 PM"
+            }, {
+                name: "10:15 PM",
+                value: "10:15 PM"
+            }, {
+                name: "10:30 PM",
+                value: "10:30 PM"
+            }, {
+                name: "10:45 PM",
+                value: "10:45 PM"
+            }, {
+                name: "11:00 PM",
+                value: "11:00 PM"
+            }, {
+                name: "11:15 PM",
+                value: "11:15 PM"
+            }, {
+                name: "11:30 PM",
+                value: "11:30 PM"
+            }, {
+                name: "11:45 PM",
+                value: "11:45 PM"
+            }];
+
+        }, 
+        getToTimeOptions:function(){
+        	//ToDo: Logic to show only greater values from From Time.
+        	return [{
+                name: "12:00 AM",
+                value: "12:00 AM", 
+                id: 0
+            }, {
+                name: "12:15 AM",
+                value: "12:15 AM"
+            }, {
+                name: "12:30 AM",
+                value: "12:30 AM"
+            }, {
+                name: "12:45 AM",
+                value: "12:45 AM"
+            }, {
+                name: "1:00 AM",
+                value: "1:00 AM"
+            }, {
+                name: "1:15 AM",
+                value: "1:15 AM"
+            }, {
+                name: "1:30 AM",
+                value: "1:30 AM"
+            }, {
+                name: "1:45 AM",
+                value: "1:45 AM"
+            }, {
+                name: "2:00 AM",
+                value: "2:00 AM"
+            }, {
+                name: "2:15 AM",
+                value: "2:15 AM"
+            }, {
+                name: "2:30 AM",
+                value: "2:30 AM"
+            }, {
+                name: "2:45 AM",
+                value: "2:45 AM"
+            }, {
+                name: "3:00 AM",
+                value: "3:00 AM"
+            }, {
+                name: "3:15 AM",
+                value: "3:15 AM"
+            }, {
+                name: "3:30 AM",
+                value: "3:30 AM"
+            }, {
+                name: "3.45 AM",
+                value: "3.45 AM"
+            }, {
+                name: "4:00 AM",
+                value: "4:00 AM"
+            }, {
+                name: "4:15 AM",
+                value: "4:15 AM"
+            }, {
+                name: "4:30 AM",
+                value: "4:30 AM"
+            }, {
+                name: "4:45 AM",
+                value: "4:45 AM"
+            }, {
+                name: "5:00 AM",
+                value: "5:00 AM"
+            }, {
+                name: "5:15 AM",
+                value: "5:15 AM"
+            }, {
+                name: "5:30 AM",
+                value: "5:30 AM"
+            }, {
+                name: "5:45 AM",
+                value: "5:45 AM"
+            }, {
+                name: "6:00 AM",
+                value: "6:00 AM"
+            }, {
+                name: "6:15 AM",
+                value: "6:15 AM"
+            }, {
+                name: "6:30 AM",
+                value: "6:30 AM"
+            }, {
+                name: "6:45 AM",
+                value: "6:45 AM"
+            }, {
+                name: "7:00 AM",
+                value: "7:00 AM"
+            }, {
+                name: "7:15 AM",
+                value: "7:15 AM"
+            }, {
+                name: "7:30 AM",
+                value: "7:30 AM"
+            }, {
+                name: "7:45 AM",
+                value: "7.45 AM"
+            }, {
+                name: "8:00 AM",
+                value: "8:00 AM"
+            }, {
+                name: "8:15 AM",
+                value: "8:15 AM"
+            }, {
+                name: "8:30 AM",
+                value: "8:30 AM"
+            }, {
+                name: "8:45 AM",
+                value: "8:45 AM"
+            }, {
+                name: "9:00 AM",
+                value: "9:00 AM"
+            }, {
+                name: "9:30 AM",
+                value: "9:30 AM"
+            }, {
+                name: "9:45 AM",
+                value: "9:45 AM"
+            }, {
+                name: "10:00 AM",
+                value: "10:00 AM"
+            }, {
+                name: "10:15 AM",
+                value: "10:15 AM"
+            }, {
+                name: "10:30 AM",
+                value: "10:30 AM"
+            }, {
+                name: "10:45 AM",
+                value: "10:45 AM"
+            }, {
+                name: "11:00 AM",
+                value: "11:00 AM"
+            }, {
+                name: "11:15 AM",
+                value: "11:15 AM"
+            }, {
+                name: "11:30 AM",
+                value: "11:30 AM"
+            }, {
+                name: "11:45 AM",
+                value: "11:45 AM"
+            }, {
+                name: "12:00 PM",
+                value: "12:00 PM"
+            }, {
+                name: "12:15 PM",
+                value: "12:15 PM"
+            }, {
+                name: "12:30 PM",
+                value: "12:30 PM"
+            }, {
+                name: "12:45 PM",
+                value: "12:45 PM"
+            }, {
+                name: "1:00 PM",
+                value: "1:00 PM"
+            }, {
+                name: "1:15 PM",
+                value: "1:15 PM"
+            }, {
+                name: "1:30 PM",
+                value: "1:30 PM"
+            }, {
+                name: "1:45 PM",
+                value: "1:45 PM"
+            }, {
+                name: "2:00 PM",
+                value: "2:00 PM"
+            }, {
+                name: "2:15 PM",
+                value: "2:15 PM"
+            }, {
+                name: "2:30 PM",
+                value: "2:30 PM"
+            }, {
+                name: "2:45 PM",
+                value: "2:45 PM"
+            }, {
+                name: "3:00 PM",
+                value: "3:00 PM"
+            }, {
+                name: "3:15 PM",
+                value: "3:15 PM"
+            }, {
+                name: "3:30 PM",
+                value: "3:30 PM"
+            }, {
+                name: "3.45 PM",
+                value: "3.45 PM"
+            }, {
+                name: "4:00 PM",
+                value: "4:00 PM"
+            }, {
+                name: "4:15 PM",
+                value: "4:15 PM"
+            }, {
+                name: "4:30 PM",
+                value: "4:30 PM"
+            }, {
+                name: "4:45 PM",
+                value: "4:45 PM"
+            }, {
+                name: "5:00 PM",
+                value: "5:00 PM"
+            }, {
+                name: "5:15 PM",
+                value: "5:15 PM"
+            }, {
+                name: "5:30 PM",
+                value: "5:30 PM"
+            }, {
+                name: "5:45 PM",
+                value: "5:45 PM"
+            }, {
+                name: "6:00 PM",
+                value: "6:00 PM"
+            }, {
+                name: "6:15 PM",
+                value: "6:15 PM"
+            }, {
+                name: "6:30 PM",
+                value: "6:30 PM"
+            }, {
+                name: "6:45 PM",
+                value: "6:45 PM"
+            }, {
+                name: "7:00 PM",
+                value: "7:00 PM"
+            }, {
+                name: "7:15 PM",
+                value: "7:15 PM"
+            }, {
+                name: "7:30 PM",
+                value: "7:30 PM"
+            }, {
+                name: "7:45 PM",
+                value: "7.45 PM"
+            }, {
+                name: "8:00 PM",
+                value: "8:00 PM"
+            }, {
+                name: "8:15 PM",
+                value: "8:15 PM"
+            }, {
+                name: "8:30 PM",
+                value: "8:30 PM"
+            }, {
+                name: "8:45 PM",
+                value: "8:45 PM"
+            }, {
+                name: "9:00 PM",
+                value: "9:00 PM"
+            }, {
+                name: "9:30 PM",
+                value: "9:30 PM"
+            }, {
+                name: "9:45 PM",
+                value: "9:45 PM"
+            }, {
+                name: "10:00 PM",
+                value: "10:00 PM"
+            }, {
+                name: "10:15 PM",
+                value: "10:15 PM"
+            }, {
+                name: "10:30 PM",
+                value: "10:30 PM"
+            }, {
+                name: "10:45 PM",
+                value: "10:45 PM"
+            }, {
+                name: "11:00 PM",
+                value: "11:00 PM"
+            }, {
+                name: "11:15 PM",
+                value: "11:15 PM"
+            }, {
+                name: "11:30 PM",
+                value: "11:30 PM"
+            }, {
+                name: "11:45 PM",
+                value: "11:45 PM"
+            }];
+
+        }
+    }
+});
